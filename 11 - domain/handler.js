07bd7d18a3f9b9ffea30adf16a5d98515873b63b
leshikhacker <leshikhacker@gmail.com>
2016-01-04 07:39:17 +0200

    domain

---
+++ 11 - domain/handler.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+module.exports = function handler(req, res) {
+  if(req.url == '/') {
+    fs.readFile('index1.html', function(err, content) {
+      if(err) throw err;
+      res.end(content);
+    });
+  }
+  else {
+    res.statusCode = 404;
+    res.end('Not Found!');
+  }
+};
